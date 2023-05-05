2ee3e041557a0abfdfdcbabf570e9f8e9e82be23
Hippo Maru <hippomaru@ya.ru>
2023-05-05 12:57:29 +0300

    lesson 4 added

---
+++ src/com/itproger/Lesson_4.java
@@ -0,0 +1,23 @@
+package com.itproger;
+
+public class Lesson_4 {
+    // https://www.youtube.com/watch?v=mTucAnRpxcQ&list=PLDyJYA6aTY1lT614ixLYq48har7EnCXpk&index=4
+    public static void main(String[] args){
+        //integers
+        byte b1 = -128;
+        byte b2 = 127;
+        short s1 = -32768;
+        short s2 = 32767;
+        int i = 4;
+        long l = 8;
+        //floats
+        float f = 3.1415926f;
+        double d = 3.14159261415926;
+        //strings
+        char ch = 'x';
+        String s = "solve et coagula";
+        System.out.println(s);
+        //bool
+        boolean False = true; // :)
+    }
+}

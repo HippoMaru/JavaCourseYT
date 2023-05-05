a05959f1df43476e2869b55370418dd6eb8c9a1e
Hippo Maru <hippomaru@ya.ru>
2023-05-05 12:42:33 +0300

    Initial commit

---
+++ src/com/itproger/Lesson_3.java
@@ -0,0 +1,12 @@
+package com.itproger;
+
+public class Lesson_3 {
+
+    public static void main(String[] args){
+        // https://www.youtube.com/watch?v=CEMhFQZrVzw&list=PLDyJYA6aTY1lT614ixLYq48har7EnCXpk&index=3
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("!");
+    }
+
+}
